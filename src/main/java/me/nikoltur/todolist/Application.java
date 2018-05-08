3a06f93c2a399aa90260ab4844cb0928bff66de1
Nikolas Turunen <nikoltur@gmail.com>
2018-05-09 01:39:03 +0300

    Improved logging, Changed version to 0.5.1

--- src/main/java/me/nikoltur/todolist/Application.java
+++ src/main/java/me/nikoltur/todolist/Application.java
@@ -1,9 +1,11 @@
 package me.nikoltur.todolist;
 
+import me.nikoltur.springglobalconfiguration.GlobalHttpRequestLoggingConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -14,6 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
+@Import(GlobalHttpRequestLoggingConfiguration.class)
 public class Application {
 
     /**
